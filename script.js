14f6171f5a37c3b658d0dce5a7d1093d9b6f2a4c
Ваше Имя <you@example.com>
2020-11-30 01:25:42 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const calculator = document.querySelector(".calculator");
+const [valueA, valueB, result] = document.querySelectorAll(".input__value")
+
+function calculatePlus() {
+    result.value = parseFloat(valueA.value) + parseFloat(valueB.value)
+}
+
+function calculateMinus() {
+    result.value = parseFloat(valueA.value) - parseFloat(valueB.value)
+}
+
+function calculateMultiply() {
+    result.value = parseFloat(valueA.value) * parseFloat(valueB.value)
+}
+
+function calculateDivision() {
+    result.value = parseFloat(valueA.value) / parseFloat(valueB.value)
+}
+
+function calculateRemainderOfDivision() {
+    result.value = parseFloat(valueA.value) % parseFloat(valueB.value)
+}
+
+
+calculator.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    console.log(event);
+
+    switch (event.submitter.dataset.action) {
+        case "plus" : return calculatePlus()
+        case "minus" : return calculateMinus()
+        case "multiply" : return calculateMultiply()
+        case "division" : return calculateDivision()
+        case "remainder-of-division" : return calculateRemainderOfDivision()
+    }
+})
